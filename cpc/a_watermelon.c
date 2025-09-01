c32e7815fbb30606c17d829c5dd407ad1484d478
Kawsar <khsbd2@gmail.com>
2025-09-01 22:28:22 +0600

    formatting unformat

--- cpc/a_watermelon.c
+++ cpc/a_watermelon.c
@@ -1,6 +1,2 @@
 #include<stdio.h>
-void main(){
-    int w;
-    scanf("%d",&w);
-    printf("%s\n",(w>2&&w%2==0)?"YES":"NO");
-}
+void main(){int w;scanf("%d",&w);printf("%s\n",(w>2&&w%2==0)?"YES":"NO");}
