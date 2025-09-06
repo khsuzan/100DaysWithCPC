8af0489bcb7415faf4fd19f414f8810bb7be98f4
Kawsar <khsbd2@gmail.com>
2025-09-07 00:00:27 +0600

    announces section

--- univus/lib/features/dashboard/view/dashboard_screen.dart
+++ univus/lib/features/dashboard/view/dashboard_screen.dart
@@ -24,8 +24,8 @@ class _DashboardScreenState extends State<DashboardScreen> {
             _headerBar(),
             _gap(20.h),
             _upComingClasses(),
-            // _gap(20.h),
-            // _upComingClasses(),
+            _gap(20.h),
+            _announces(),
           ],
         ),
       ),
@@ -176,25 +176,34 @@ class _DashboardScreenState extends State<DashboardScreen> {
     );
   }
 
-
-
   MultiSliver _announces() {
     return MultiSliver(
       children: [
         SliverToBoxAdapter(
-          child: Text("Upcoming classes", style: TextStyle(fontSize: 18.sp)),
-        ),
-        _gap(4.h),
-        SliverToBoxAdapter(
-          child: Text(
-            "06-Sep-25",
-            style: TextStyle(fontSize: 13.sp, color: MyColors.gray),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text("Announces", style: TextStyle(fontSize: 18.sp)),
+              Spacer(),
+              InkWell(
+                onTap: () {},
+                borderRadius: BorderRadius.circular(50.h), 
+                child: Padding(
+                  padding: EdgeInsets.only(top: 4.h, bottom: 4.h, left: 4.h),
+                  child: Text(
+                    "Show All",
+                    style: TextStyle(fontSize: 13.sp, color: MyColors.gray),
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
+        _gap(4.h),
         SliverList(
           delegate: SliverChildListDelegate.fixed(
-            List.generate(5, (index) {
-              return _upComingClass();
+            List.generate(2, (index) {
+              return _announcement();
             }),
           ),
         ),
@@ -202,6 +211,61 @@ class _DashboardScreenState extends State<DashboardScreen> {
     );
   }
 
+  Widget _announcement() {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 4.h),
+      child: Container(
+        padding: EdgeInsets.all(6.h),
+        color: MyColors.backgroundLight,
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              height: 50,
+              width: 50,
+              clipBehavior: Clip.antiAlias,
+              decoration: BoxDecoration(
+                color: MyColors.accentGreen.withAlpha(50),
+                borderRadius: BorderRadius.circular(8.w),
+              ),
+              child: Image.asset("assets/images/robo.png"),
+            ),
+            SizedBox(width: 6.w),
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Text(
+                    "Campuse cloasure notice",
+                    style: TextStyle(
+                      fontSize: 16.sp,
+                      fontWeight: FontWeight.w500,
+                      color: MyColors.accentGreen,
+                    ),
+                  ),
+                  SizedBox(height: 4.h),
+                  Text.rich(
+                    TextSpan(
+                      text:
+                          "Due to inclement weather, the campus will remain close on monday 23th October...",
+                      children: [
+                        TextSpan(
+                          text: "Read More",
+                          style: TextStyle(color: MyColors.accentGreen),
+                        ),
+                      ],
+                      style: TextStyle(color: MyColors.gray),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 
   Widget _bottomNavigationBar() {
     return SafeArea(
