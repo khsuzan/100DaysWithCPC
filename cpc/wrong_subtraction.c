135a788cdab5f4208dff3f371cf34392bf1ee5fc
Kawsar <khsbd2@gmail.com>
2025-09-17 23:01:12 +0600

    https://codeforces.com/contest/977/submission/339158690

---
+++ cpc/wrong_subtraction.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+    int n, k;
+    scanf("%d %d", &n, &k);
+    for(int i = 0; i < k; i++){
+        if(n % 10 != 0){
+            n--;
+        }else{
+            n /= 10;
+        }
+    }
+    printf("%d", n);
+    return 0;
+}
